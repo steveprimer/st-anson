04a0338446a9af824261e7ef32c5ce84f23e457e
Anson Stephan <ansonstephan@gmail.com>
2024-07-13 00:47:25 +0530

    4th commit

--- week0/day2/homework2.java
+++ week0/day2/homework2.java
@@ -1,6 +1,6 @@
 package week0.day2;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class homework2 {
     public static void main(String[] args) {
@@ -53,17 +53,17 @@ public class homework2 {
         // sc.close();
         // }
 
-        // int n; INVERTED LEFT FACING TRIANGLE
-        // Scanner sc = new Scanner(System.in);
-        // n = sc.nextInt();
-        // for (int i = n; i >= 1; i--) {
-        // for (int k = 1; k <= n - i; k++)
-        // System.out.print(" ");
-        // for (int j = 1; j <= i; j++)
-        // System.out.print(" *");
-        // System.out.println();
-        // sc.close();
-        // }
+        int n; // INVERTED LEFT FACING TRIANGLE
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        for (int i = n; i >= 1; i--) {
+            for (int k = 1; k <= n - i; k++)
+                System.out.print(" ");
+            for (int j = 1; j <= i; j++)
+                System.out.print(" *");
+            System.out.println();
+            sc.close();
+        }
 
     }
 
